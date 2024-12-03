5d9c27beb7f7ca9eb359c77ce4d08701c740c728
182421Kjel! <kjelpw@protonmail.com>
2024-12-03 15:41:20 -0700

    counter popup on nav menu

--- skyhook_timer/auth_hooks.py
+++ skyhook_timer/auth_hooks.py
@@ -1,8 +1,11 @@
 from django.utils.translation import gettext_lazy as _
+from django.utils.timezone import now
+from datetime import timedelta
 
 from allianceauth import hooks
 from allianceauth.services.hooks import MenuItemHook, UrlHook
 
+from .models import SkyhookTimer
 from . import urls
 
 
@@ -19,8 +22,18 @@ class SkyhookTimerMenuItem(MenuItemHook):
             navactive=["skyhook_timer:"],
         )
 
+    def _calculate_count_for_user(self):
+        """Calculate the number of timers with time remaining within 1 hour."""
+        one_hour_from_now = now() + timedelta(hours=1)
+        vulnerable_skyhooks = SkyhookTimer.objects.filter(
+            countdown_time__lte=one_hour_from_now,  # Time within the next hour
+            countdown_time__gt=now()               # Time still in the future
+        ).count()
+        return vulnerable_skyhooks if vulnerable_skyhooks > 0 else None
+
     def render(self, request):
         if request.user.has_perm('skyhook_timer.view_skyhooktimer'):
+            self.count = self._calculate_count_for_user()
             return MenuItemHook.render(self, request)
         return ''
 
