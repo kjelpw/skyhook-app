3e2d171d7a9c336bff55e35ece381ab337d32ab9
182421Kjel! <kjelpw@protonmail.com>
2024-12-04 09:16:39 -0700

    form update

--- skyhook_timer/forms.py
+++ skyhook_timer/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.utils.timezone import now, timedelta
+
 from skyhook_timer.models import SkyhookTimer
 
 class SkyhookTimerForm(forms.ModelForm):
@@ -14,3 +16,23 @@ class SkyhookTimerForm(forms.ModelForm):
                 'class': 'form-control',
             }),
         }
+
+        def clean(self):
+            cleaned_data = super().clean()
+            days = cleaned_data.get("days", 0)
+            hours = cleaned_data.get("hours", 0)
+            minutes = cleaned_data.get("minutes", 0)
+            
+            if days == 0 and hours == 0 and minutes == 0:
+                raise forms.ValidationError("Time remaining cannot be zero.")
+            
+            countdown_time = now() + timedelta(days=days, hours=hours, minutes=minutes)
+            cleaned_data['countdown_time'] = countdown_time
+            return cleaned_data
+        
+        def save(self, commit=True):
+            instance = super().save(commit=False)
+            instance.countdown_time = self.cleaned_data['countdown_time']
+            if commit:
+                instance.save()
+            return instance
